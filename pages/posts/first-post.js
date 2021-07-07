c9a7fe2991912671c5ee020ae8f4d4f2438a381d
euller <eullerz01@hotmail.com>
2021-07-07 19:52:41 -0300

    Styled routes.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 
 export default function FirstPost() {
 
   return (
     <>
+    <Layout>
         <Head>
           <title>First Post</title>
         </Head>
@@ -16,6 +18,8 @@ export default function FirstPost() {
             <a>Back to Home</a>
           </Link>
         </h2>
+    </Layout>
+    
     </>
   )
 }
